227891b5e486542c4a89d9d13bd65c5474beed50
Kevin Whitley <krwhitley@gmail.com>
2019-10-02 13:58:25 -0500

    released v1.7.4 - fix:rollup was not including external ref to use-store-hook

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import typescript from 'rollup-plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 
 export default {
   input: 'src/index.js',
@@ -9,9 +10,12 @@ export default {
     format: 'cjs',
   },
   plugins: [
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
     resolve(),
     typescript({ module: 'CommonJS' }),
     commonjs({ extensions: ['.js', '.ts', '.tsx'] }), // the ".ts" extension is required
   ],
-  external: ['react', 'react-dom', 'prop-types'],
+  external: ['react', 'react-dom', 'use-store-hook'],
 }
